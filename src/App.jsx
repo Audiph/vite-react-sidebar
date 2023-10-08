9c2a5ad943c04592fd25f2d4728e203efc7e8184
Jeff Lagarde <lagarde.marcjoseph@gmail.com>
2023-10-09 02:48:07 +0800

    vite react boilerplate, install dependencies, setup base components

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Fragment } from 'react';
+import { Home, Modal, SideNav } from './components';
+
+const App = () => {
+  return (
+    <Fragment>
+      <Home />
+      <Modal />
+      <SideNav />
+    </Fragment>
+  );
+};
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import Home from './Home';
+import Modal from './Modal';
+import SideNav from './SideNav';
+
+export { Home, Modal, SideNav };
